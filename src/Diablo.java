df5842a649e8cbcce1fe0af07d642598fb73f8b9
Martin Aleksandrov <73018624+santush87@users.noreply.github.com>
2022-10-31 18:42:53 +0100

    First SpringData commit

---
+++ src/Diablo.java
@@ -0,0 +1,39 @@
+import java.sql.*;
+import java.util.Properties;
+import java.util.Scanner;
+
+public class Diablo {
+
+    public static void main(String[] args) throws SQLException {
+        Scanner scanner = new Scanner(System.in);
+
+        Properties properties = new Properties();
+        properties.setProperty("user", "root");
+        properties.setProperty("password", "8705");
+
+       Connection connection = DriverManager
+               .getConnection("jdbc:mysql://localhost:3306/diablo", properties);
+
+        System.out.println("Enter username: ");
+        String username = scanner.nextLine();
+
+        PreparedStatement query =
+                connection.prepareStatement("SELECT user_name, first_name, last_name, " +
+                        "(SELECT COUNT(*) FROM users_games WHERE user_id = u.id) AS games_count" +
+                        " FROM users AS u" +
+                        " WHERE user_name = ?");
+        query.setString(1, username);
+
+        ResultSet result = query.executeQuery();
+
+        if (result.next()){
+            String dbUserName = result.getString("user_name");
+            String dbFirstName = result.getString("first_name");
+            String dbLastName = result.getString("last_name");
+            int gamesCount = result.getInt("games_count");
+            System.out.printf("User: %s%n%s %s has played %d games", dbUserName, dbFirstName, dbLastName, gamesCount);
+        }else {
+            System.out.println("No such user exists");
+        }
+    }
+}
