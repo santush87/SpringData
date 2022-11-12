49fced08faec8683134a71fe542d8c8e714d47cd
Martin Aleksandrov <73018624+santush87@users.noreply.github.com>
2022-11-12 14:50:43 +0100

    2.Change casing

---
+++ IntroToHibernateExercise/src/main/java/ChangeCasing.java
@@ -0,0 +1,34 @@
+import entities.Town;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+import java.util.List;
+
+public class ChangeCasing {
+    private static final String DATABASE_NAME = "soft-uni";
+    private static final String UPDATE_ALL_TOWNS_WITH_LENGTH_MORE_THAN_5
+            = "UPDATE Town t SET t.name = UPPER(t.name) WHERE LENGTH(t.name) > 5";
+
+    public static void main(String[] args) {
+        final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory(DATABASE_NAME);
+
+        final EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        entityManager.getTransaction().begin();
+
+        Query towns = entityManager.createQuery("SELECT t FROM Town t", Town.class);
+
+        List<Town> resultList = towns.getResultList();
+
+        for (Town town : resultList) {
+            if (town.getName().length() <= 5) {
+                town.setName(town.getName().toUpperCase());
+                entityManager.persist(town);
+            }
+        }
+
+        entityManager.getTransaction().commit();
+    }
+}
