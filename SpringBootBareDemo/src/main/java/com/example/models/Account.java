d28dacfcb17efa63af69e3df5aa88ea8df88ca1a
Martin Aleksandrov <73018624+santush87@users.noreply.github.com>
2022-11-14 09:22:43 +0100

    Spring Boot Bare Demo

---
+++ SpringBootBareDemo/src/main/java/com/example/models/Account.java
@@ -0,0 +1,37 @@
+package com.example.models;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "accounts")
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private BigDecimal balance;
+
+//    private User user;
+
+    public Account() {
+        this.balance = BigDecimal.ZERO;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+}
---
+++ SpringBootBareDemo/src/main/java/com/example/services/UserService.java
@@ -0,0 +1,5 @@
+package com.example.services;
+
+public interface UserService {
+    void register(String username, int age);
+}
