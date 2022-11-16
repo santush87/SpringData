9b7d62b2f49aac644c3c1cbabc90ec68fe0a7506
Martin Aleksandrov <73018624+santush87@users.noreply.github.com>
2022-11-16 10:21:52 +0100

    AddressXmlDto creating

--- ModelMapper/src/main/java/com/example/modelmapper/entities/dtos/AddressXmlDto.java
+++ ModelMapper/src/main/java/com/example/modelmapper/entities/dtos/AddressXmlDto.java
@@ -1,4 +1,37 @@
 package com.example.modelmapper.entities.dtos;
 
 public class AddressXmlDto {
+    private int id;
+    private String country;
+    private String city;
+
+    public AddressXmlDto(int id, String country, String city) {
+        this.id = id;
+        this.country = country;
+        this.city = city;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
 }
