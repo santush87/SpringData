198686f44baeeb61353de3c24922a592125c892f
Martin Aleksandrov <73018624+santush87@users.noreply.github.com>
2022-11-23 09:41:51 +0100

    commit

---
+++ IntroToHibernateExercise/src/main/java/Utils.java
@@ -0,0 +1,10 @@
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+
+enum MyUtils {
+//    ;
+//    public EntityManager createEntityManager() {
+//        return Persistence.createEntityManagerFactory("soft-uni")
+//                .createEntityManager();
+//    }
+}
